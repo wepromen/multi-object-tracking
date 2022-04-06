c77905b23d3c898e8ef83262844c61e45f73f6c0
Nam Nguyen <namnh@unicloud.com.vn>
2022-04-06 16:27:46 +0700

    multi proc + multi thread + fps 1.6 slow 50%

---
+++ tracking_multi_procs.py
@@ -0,0 +1,99 @@
+from __future__ import division, print_function, absolute_import
+from sys import maxsize
+
+from timeit import time
+import warnings
+import cv2
+from mot_woker import MOTWorker
+
+from utils.video_shower import VideoShower
+
+from multiprocessing import Process, Value, Queue
+
+warnings.filterwarnings('ignore')
+
+mot_worker_input_queue = Queue()
+showerBBQueue = Queue()
+fpsQueue = Queue()
+showerFrameQueue = Queue()
+
+
+def bbox_transform(newbboxs):
+    return_boxs = []
+    for i in range(newbboxs.shape[0]):
+        [x,y,w,h] = [newbboxs[i,0,0],newbboxs[i,0,1],newbboxs[i,3,0]-newbboxs[i,0,0],newbboxs[i,3,1]-newbboxs[i,0,1]]
+        return_boxs.append([x,y,w,h])
+    return return_boxs
+
+def main():
+    ## get video source
+    video_filename = -1
+    # video_filename = './samples/u_frontcam_cuted_853x480.mp4'
+    video_capture = cv2.VideoCapture(video_filename)
+
+    #initial shower
+    showerVideo = Value('i', 1)
+    global showerBBQueue
+    global showerFrameQueue
+    global fpsQueue
+    global mot_worker_input_queue
+   
+    videoShower = VideoShower()
+    videoShowerProcess = Process(target=videoShower.start, args=(showerVideo, showerFrameQueue, showerBBQueue, fpsQueue))
+    videoShowerProcess.start()
+
+    # NOTE: MOT Workers
+    number_of_mot_workers = 1
+
+    for i in range(number_of_mot_workers):
+        mot_worker = MOTWorker(input_queue=mot_worker_input_queue,
+                                output_queue=showerBBQueue, name= '@@ MOTWorker ' + str(i), fpsQueue = fpsQueue)
+        mot_worker.daemon = True
+        print(mot_worker)
+        mot_worker.start()
+
+
+    while True:
+        ok, frame = video_capture.read()  # frame shape 640*480*3
+        if ok != True:
+            break;
+
+        if showerFrameQueue.qsize() <= 1:
+            showerFrameQueue.put(frame)
+            # print('@@ === Put frame to shower Proc')
+        
+        if mot_worker_input_queue.qsize() <= 1:
+            mot_worker_input_queue.put_nowait(frame)
+            # print('@@ === Put frame qsize: ', mot_worker_input_queue.qsize())
+        
+        
+    # frame_rate = 10000
+    # prev = 0
+
+    # while True:
+    #     time_elapsed = time.time() - prev
+    #     ok, frame = video_capture.read()  # frame shape 640*480*3
+    #     if ok != True:
+    #         break;
+
+    #     if time_elapsed > 1./frame_rate:
+    #         prev = time.time()
+
+    #         if videoShowerProcess is not None:
+    #             showerFrameQueue.put(frame)
+    #             # print('@@ === Put frame to shower Proc')
+
+    #         mot_worker_input_queue.put(frame)
+
+    video_capture.release()
+
+    if videoShowerProcess is not None:
+        showerVideo.value = 0
+        videoShowerProcess.join()
+    if mot_worker is not None:
+        mot_worker.join()
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main() 
--- tracking_multi_threads.py
+++ tracking_multi_threads.py
@@ -1,7 +1,8 @@
 from __future__ import division, print_function, absolute_import
 from sys import maxsize
 
-from timeit import time
+# from timeit import time
+import time
 import warnings
 import cv2
 from mot_woker import MOTWorker
@@ -20,9 +21,10 @@ model = YOLO()
 input_playback_f_q = Queue()
 input_mot_f_q = Queue()
 output_bbox_q = Queue()
+output_fps_q = Queue()
 output_playback_q = Queue()
-
-
+oldBBox = None
+oldFPS = None
 
 def frameGetter():
     video_filename = -1
@@ -36,16 +38,21 @@ def frameGetter():
             break;
 
         if (input_mot_f_q.qsize() <= 1):
-            input_mot_f_q.put(frame)
+            input_mot_f_q.put_nowait(frame)
             # print('@@ input_mot_f_q qsize: ', input_mot_f_q.qsize())
 
         if (input_playback_f_q.qsize() <= 1):
-            input_playback_f_q.put(frame)
+            input_playback_f_q.put_nowait(frame)
             # print('@@ input_playback_f_q qsize: ', input_playback_f_q.qsize())
     video_capture.release()
 
 def playbackVideo():
     global input_playback_f_q
+    global output_bbox_q
+    global output_fps_q
+    global oldBBox
+    global oldFPS
+
     win_name = "output"
     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
 
@@ -53,8 +60,29 @@ def playbackVideo():
         if input_playback_f_q.empty() == True:
             continue;
 
-        frame = input_playback_f_q.get()
+        frame = input_playback_f_q.get_nowait()
         # print('@@ playback_thread frame: ', frame)
+
+        if not output_bbox_q.empty():
+            # print('@@ output_bbox_q: ', output_bbox_q.get())
+            (xmin, ymin, boxw, boxh) = output_bbox_q.get()
+            # print('@@ shower Prc bbox: ', xmin, ymin, boxw, boxh)
+            cv2.rectangle(frame, (xmin,ymin), (xmin+boxw,ymin+boxh), (255,0,255), 2) # BGR color
+            oldBBox = (xmin, ymin, boxw, boxh)
+        else:
+            if (oldBBox is not None):
+                (xmin, ymin, boxw, boxh) = oldBBox
+                # print('@@ shower Prc bbox: ', xmin, ymin, boxw, boxh)
+                cv2.rectangle(frame, (xmin,ymin), (xmin+boxw,ymin+boxh), (255,0,255), 2) # BGR color
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if not output_fps_q.empty():
+            fps = output_fps_q.get()
+            cv2.putText(frame, fps, (10, 50), font, 2.0, (255, 0, 255), 2)
+            oldFPS = fps
+        else:
+            cv2.putText(frame, oldFPS, (10, 50), font, 2.0, (255, 0, 255), 2)
+
         cv2.imshow(win_name, frame)
         cv2.resizeWindow(win_name, 960, 540)
 
@@ -66,15 +94,20 @@ def playbackVideo():
 
 def detector2(frame):
     global model
+    global output_bbox_q
     if frame is not None:
         image = Image.fromarray(frame)
-        # if (model == None):
-        #     model = YOLO()
-        t1 = time.time()
+        t2 = time.time()
         dets = model.detect_image(image)
-        print('@@ Det time: ', time.time() - t1)
+        print('@@ Det time: ', time.time() - t2)
         if (len(dets) > 0):
-            print('@@ Dets: ', dets[0])
+            if isinstance(dets[0], list):
+                for i in dets:
+                    if (output_bbox_q.qsize() <= 1):
+                        output_bbox_q.put(i)
+            # else:
+            #     output_bbox_q.put(dets)
+        #     print('@@ Dets: ', dets[0])
 
 def main():
     frame_getter_thread = threading.Thread(target=frameGetter, args=())
@@ -83,24 +116,22 @@ def main():
     frame_getter_thread.start()
     # frame_getter_thread.join()
     playback_thread.start()
-
-    # global input_mot_f_q
+    fps = 0.0
+    global output_fps_q
 
     while True:
         if input_mot_f_q.empty() == True:
             continue;
 
         frame = input_mot_f_q.get()
+        t1 = time.time()
         detector2(frame)
-
-     # for i in range(1):
-        #    model = YOLO()
-        #    thr = threading.Thread(target=detector2, args=(frame, model))
-        #    thr.start()
-        #    thr.join()   
-
-        # if dets is not None:
-        #     print('@@ dets: ', dets)
+        t_detec_end = time.time()
+        fps  = ( fps + (1./(t_detec_end-t1)) ) / 2
+        print("fps= %f"%(fps))
+        textFPS = 'FPS: {:.2f}'.format(fps)
+        if (output_fps_q.qsize() <= 1):
+            output_fps_q.put(textFPS)
 
 if __name__ == '__main__':
     main() 
